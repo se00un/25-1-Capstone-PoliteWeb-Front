1d86421f7b5f996632529be45ba87ba17ca25fbc
박서은 <seoeun@bagseoeun-ui-MacBookPro.local>
2025-08-08 01:50:11 +0900

    api modify

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,9 +2,14 @@
 import axios from "axios";
 
 const API_BASE = import.meta.env.VITE_API_BASE_URL;
-
+console.log("API_BASE:", API_BASE);  
 const api = axios.create({
   baseURL: API_BASE,
+});
+
+api.interceptors.request.use(config => {
+  console.log("요청 URL:", config.baseURL + config.url);
+  return config;
 });
 
 export default api;
